d9f2054a8ca081cd1aee4ceaab9b2eb5e8814394
ihorkoi <koleev.igor@gmail.com>
2025-07-14 15:56:31 +0300

    add tries

--- pb1/guesser.py
+++ pb1/guesser.py
@@ -1,18 +1,23 @@
 import random
 
 
-def guess_number(secret_number, guess):
-    while True:
+def guess_number(secret_number, guess, tries):
+    fails = 0
+    while fails < tries:
         if guess < secret_number:
-            guess = int(input("Too low!:"))
+            fails += 1
+            guess = int(input(f"Too low! You have {tries-fails} left!:"))
         if guess > secret_number:
-            guess = int(input("Too high!:"))
+            fails += 1
+            guess = int(input(f"Too high! You have {tries-fails} left!:"))
         if guess == secret_number:
             print("Congratulations! You've guessed the number!")
             break
+    print('You lost! Try your luck again')
 
 
 if __name__ == "__main__":
     secret_number = random.randint(1, 100)
+    tries = 7
     guess = int(input("Guess a number between 1 and 100: "))
-    guess_number(secret_number, guess)
+    guess_number(secret_number, guess, tries)
