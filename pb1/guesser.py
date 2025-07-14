8ff8f8e86b65568bf684e8eaf592c39b752315a9
ihorkoi <koleev.igor@gmail.com>
2025-07-14 16:20:08 +0300

    add error handler

--- pb1/guesser.py
+++ pb1/guesser.py
@@ -1,23 +1,28 @@
 import random
 
 
-def guess_number(secret_number, guess, tries):
+def guess_number(secret_number, tries):
     fails = 0
     while fails < tries:
+        try:
+            guess = int(
+                input(f"Guess a number between 1 and 100. {tries-fails} attempts left: "))
+        except:
+            print('Invalid number!')
+            continue
         if guess < secret_number:
             fails += 1
-            guess = int(input(f"Too low! You have {tries-fails} left!:"))
-        if guess > secret_number:
+            print("Too low!")
+        elif guess > secret_number:
             fails += 1
-            guess = int(input(f"Too high! You have {tries-fails} left!:"))
-        if guess == secret_number:
+            print("Too high!")
+        else:
             print("Congratulations! You've guessed the number!")
-            break
+            return
     print('You lost! Try your luck again')
 
 
 if __name__ == "__main__":
     secret_number = random.randint(1, 100)
     tries = 7
-    guess = int(input("Guess a number between 1 and 100: "))
-    guess_number(secret_number, guess, tries)
+    guess_number(secret_number, tries)
