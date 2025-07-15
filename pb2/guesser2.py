cfc554a84527d16aca659ae897cd02422a317fa1
ihorkoi <koleev.igor@gmail.com>
2025-07-15 17:09:43 +0300

    added input check and rewrite word display

--- pb2/guesser2.py
+++ pb2/guesser2.py
@@ -3,15 +3,28 @@ import random
 
 def word_guesser(selected_word, tries):
     word = '*' * len(selected_word)
+    entered_letters = set()
     guesses = 0
     while guesses < tries:
-        letter = input('Try to guess a word! Type a letter:')
-        for letter_idx in range(len(selected_word)):
-            if selected_word[letter_idx] == letter:
-                word = list(word)
-                word[letter_idx] = letter
-                word = ''.join(word)
+        try:
+            letter = input('Try to guess a word! Type a letter:')
+            if not letter.isalpha() or len(letter) < 1:
+                print('Please, provide a correct input')
+                continue
+            if letter in entered_letters:
+                print('You have already tried this letter!')
+                continue
+            else:
+                entered_letters.add(letter)
+        except:
+            print('Enter a valid letter!')
+            continue
+
+        word = ''.join(letter if letter == selected_word[i] else word[i]
+                       for i in range(len(selected_word)))
+
         print(word)
+        print(f'You have {tries - guesses-1} tries left!')
         guesses += 1
         if word == selected_word:
             print('Winner!')
@@ -22,6 +35,6 @@ def word_guesser(selected_word, tries):
 if __name__ == "__main__":
     words_list = ["go", "python", "java", "javascript", "ruby", "html", "css", "swift", "kotlin", "typescript", "csharp", "php", "sql",
                   "rust", "dart", "scala", "elixir", "clojure", "haskell", "erlang", "groovy", "perl", "lua", "bash", "powershell", "matlab", "r", "julia"]
-    selected_word = words_list[random.randint(0, len(words_list))]
+    selected_word = random.choice(words_list)
     tries = 7
     word_guesser(selected_word, tries)
